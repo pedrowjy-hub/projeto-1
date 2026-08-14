6fecabf0edfa705524562b78f526a2269a2e3428
Pedro Wu <pedro.wjy@gmail.com>
2026-08-14 14:02:45 -0300

    arquivo para criar banco de dados

---
+++ createdb.py
@@ -0,0 +1,22 @@
+import sqlite3
+import json
+
+con = sqlite3.connect('banco.db')
+cur = con.cursor()
+cur.execute('''
+    CREATE TABLE note(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    content TEXT)
+''')
+
+with open('notes.json','r', enconding = 'utf-8') as arquivo:
+    notas = json.load(arquivo)
+
+    cur.executemany(
+        'INSERT INTO note (title, content), (?,?)',
+        [(nota['titulo'],nota['detalhes']) for nota in notas]
+    )
+
+con.commit()
+con.close()
